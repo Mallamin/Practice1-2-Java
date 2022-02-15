0283b6b31d9b74b1d0c5543377f0fac03fd7be7c
mohamed.jalloh <mohamed.jalloh@WEPLUS97.wenet.local>
2022-02-15 14:44:33 +0100

    Java basics

---
+++ src/main/java/practice/Application.java
@@ -0,0 +1,27 @@
+package practice;
+
+public class Application {
+
+	public static void main(String[] args) {
+System.out.println("Hello World!");
+// primitive types
+int myNumber=55;
+short myShort=888;
+long myLong=7890;
+double myDouble=8.09;
+byte myByte=12;
+boolean myBool=true;
+
+float myFloat=345.7f;
+System.out.println(myFloat);
+System.out.println(myDouble);
+System.out.println(myLong);
+System.out.println(myShort);
+System.out.println(myNumber);
+char myChar='M';
+System.out.println(myChar);
+System.out.println(myBool);
+System.out.println(myByte);
+	}
+
+}
